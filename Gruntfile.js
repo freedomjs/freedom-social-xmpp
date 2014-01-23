ce6d56b956532d41e113945dfaf4b30b5a4028cc
Will Scott <willscott@gmail.com>
2014-01-23 09:53:54 -0800

    more structure

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      dist: {
+        files: {
+          'node-xmpp-browser.js': ['./node_modules/node-xmpp-client/browserify.js'],
+        },
+        options: {
+          alias : 'request:browser-request',
+          ignore : ['node-stringprep', 'faye-websocket', './srv', 'dns', 'tls']
+        }
+      }
+    }
+  });
+
+  // Load tasks.
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // Default tasks.
+  grunt.registerTask('compile', [
+    'browserify'
+  ]);
+  grunt.registerTask('default', ['compile']);
+};
