7c8ca85e837b16e6451be9821bb98c9e3040d532
Lucas Dixon <ldixon@google.com>
2014-03-22 11:48:46 -0400

    removed un-needed logging

--- spec/dns.spec.js
+++ spec/dns.spec.js
@@ -8,7 +8,6 @@ describe("Tests for DNS lookup", function() {
   var fooDnsQuery = flattenDNS({query: fooHostname, type: DNSTypes.A});
   var fooDnsQueryHexString = ArrayBuffers.arrayBufferToHexString(
       fooDnsQuery);
-  console.log(fooDnsQueryHexString);
   var fooDnsResponse = {'resultCode':96,'address':dnsServer,'port':dnsPort,
         'data': ArrayBuffers.hexStringToArrayBuffer("00.02.81.83.00.01.00.00.00.01.00.00.03.66.6f.6f.00.00.01.00.01.00.00.06.00.01.00.00.03.f3.00.40.01.61.c.72.6f.6f.74.2d.73.65.72.76.65.72.73.03.6e.65.74.00.05.6e.73.74.6c.64.c.76.65.72.69.73.69.67.6e.2d.67.72.73.03.63.6f.6d.00.78.b.b1.48.00.00.07.08.00.00.03.84.00.09.3a.80.00.01.51.80") };
 
@@ -17,7 +16,6 @@ describe("Tests for DNS lookup", function() {
       flattenDNS({query: googleComHostname, type: DNSTypes.A});
   var googleDnsQueryHexString = ArrayBuffers.arrayBufferToHexString(
       googleDnsQuery);
-  console.log(googleDnsQueryHexString);
   var googleDnsResponse = {'resultCode':112,'address':dnsServer,'port':dnsPort,
         'data': ArrayBuffers.hexStringToArrayBuffer("00.02.81.80.00.01.00.05.00.00.00.00.03.77.77.77.06.67.6f.6f.67.6c.65.03.63.6f.6d.00.00.01.00.01.c0.c.00.01.00.01.00.00.01.29.00.04.4a.7d.e2.b2.c0.c.00.01.00.01.00.00.01.29.00.04.4a.7d.e2.b0.c0.c.00.01.00.01.00.00.01.29.00.04.4a.7d.e2.b4.c0.c.00.01.00.01.00.00.01.29.00.04.4a.7d.e2.b1.c0.c.00.01.00.01.00.00.01.29.00.04.4a.7d.e2.b3") };
 
