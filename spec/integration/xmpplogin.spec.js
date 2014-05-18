7ef8e1cb40ed9fa417bc58a45c8f5d340ec4b1b9
Will Scott <willscott@gmail.com>
2014-05-18 00:42:03 -0700

    gets to connection

--- spec/integration/xmpplogin.spec.js
+++ spec/integration/xmpplogin.spec.js
@@ -1,14 +1,44 @@
+var credentials = [];
+var CredentializingView = function(caller, dispatch) {
+  this.dispatchEvent = dispatch;
+};
+
+CredentializingView.prototype.open = function(view, opts, continuation) {
+  continuation();
+};
+
+CredentializingView.prototype.show = function(continuation) {
+  continuation();
+  if (credentials.length) {
+    this.dispatchEvent('message', credentials.pop());
+  }
+};
+
+CredentializingView.prototype.close = function(continuation) {
+  continuation();
+};
+
 describe('Login integration', function() {
   var freedom;
 
   beforeEach(function() {
     freedom = require('freedom-for-node').freedom;
+    console.log(fdom.link.Node);
+    fdom.apis.register('core.view', CredentializingView);
   });
 
-  it('sees presence between two friends', function(done) {
+  it('logs in', function(done) {
     var socialClient = freedom('freedom.json', {debug:true});
     socialClient.emit('relay', 'onUserProfile');
     
+    credentials.push({
+      cmd: 'auth',
+      message: {
+        userId: 'alice@xmpp.uproxy.org',
+        password: 'test'
+      }
+    });
+                       
     socialClient.emit('login', [{
       agent: 'integration',
       version: '0.1',
