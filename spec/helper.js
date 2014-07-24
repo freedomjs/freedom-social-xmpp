17df4dc375baa82729af0f4d1fed776ccaa7efbf
Raymond Cheng <ryscheng@cs.washington.edu>
2014-07-23 17:33:21 -0700

    Update to integration test convention in freedom v0.5.2

--- spec/helper.js
+++ spec/helper.js
@@ -1,1 +1,3 @@
-TEMPLATE = "/src/social.xmpp.json";
+//TEMPLATE = "/src/social.xmpp.json";
+describe("integration.single: social.xmpp.json", SOCIAL_SINGLE_INTEGRATION_SPEC.bind(this, "/src/social.xmpp.json"));
+describe("integration.double: social.xmpp.json", SOCIAL_DOUBLE_INTEGRATION_SPEC.bind(this, "/src/social.xmpp.json"));
