d3d6d9dd0c602ff35302d48db8acd780acecc99f
dborkan <dborkan@gmail.com>
2014-08-18 15:35:08 -0700

    remove trace

--- firefox-facebook-demo/lib/listen.js
+++ firefox-facebook-demo/lib/listen.js
@@ -2,7 +2,6 @@ function setupListeners(freedom, displayWorker) {
   freedom.on(displayWorker.port.emit.bind(displayWorker.port));
 
   displayWorker.port.on('login', function() {
-    console.error('dborkan: in listen.js, got login ')
     freedom.emit("login");
   });
 
