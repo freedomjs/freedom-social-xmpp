57a7b9e59788339d93c87e1fcb002c9f155dcd45
Raymond Cheng <ryscheng@cs.washington.edu>
2014-11-24 21:52:57 -0500

    removing some log statements

--- src/demo_firefox_google/lib/listen.js
+++ src/demo_firefox_google/lib/listen.js
@@ -4,17 +4,14 @@ function setupListeners(chat, displayWorker) {
   chat.on(displayWorker.port.emit.bind(displayWorker.port));
 
   displayWorker.port.on('login', function() {
-    console.log('login');
     chat.login();
   });
 
   displayWorker.port.on('logout', function() {
-    console.log('logout');
     chat.logout();
   });
 
   displayWorker.port.on('send', function(data) {
-    console.log('send: ' + JSON.stringify(data));
     chat.send(data.to, data.msg);
   });
 
