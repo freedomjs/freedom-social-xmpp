dbe80414db69e4b5ff0fa63adadbf3fcee307f18
dborkan <dborkan@gmail.com>
2015-06-03 12:51:57 -0400

    remove whitespace

--- src/core/socialprovider.js
+++ src/core/socialprovider.js
@@ -26,7 +26,6 @@ if (typeof global !== 'undefined') {
  */
 var XMPPSocialProvider = function(dispatchEvent) {
   this.dispatchEvent = dispatchEvent;
-
   var social = freedom.social ? freedom.social() : freedom();
   this.STATUS = social.STATUS;
   this.ERRCODE = social.ERRCODE;
